76258ad30dd3521fe635cdbd4e60c792c9609521
Chasay1 <livelaughlovekarenishere@gmail.com>
2020-12-07 14:06:57 -0800

    Added 10 more jokes

--- index.js
+++ index.js
@@ -20,19 +20,18 @@ const { token } = require('./config.json');
 const jokes = require('./jokes.js');
 const prefix1 = "i'm";
 const prefix2 = 'im';
-const prefix3 = 'dad';
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('DadBot is ready!');
 });
 
 client.on('message', message => {
 	var args = message.content.trim().split(/ +/);
 	if (args.slice(0, 2).join(' ').toLowerCase() === 'dad jokes') {
-		const newJoke = getRandomInt(9);
+		const newJoke = getRandomInt(19);
 		console.log(newJoke);
 		message.channel.send(jokes[newJoke]);
 	}
