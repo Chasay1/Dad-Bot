9ee85b7fccdd5e5419a954a314338a811a385d0d
Chasay1 <livelaughlovekarenishere@gmail.com>
2020-12-08 02:03:00 -0800

    Fixed encoding

--- jokes.js
+++ jokes.js
@@ -3,9 +3,9 @@ module.exports = {
     2: 'Why did the scarecrow win an award?\n||Because he was outstanding in his field.||', 3: 'Did you hear about the guy who invented the knock-knock joke?\n||He won the \'no- bell\' prize.||',
     4: 'Did you hear about the circus fire?\n||It was in tents.||', 5: 'Can February March?\n||No, but April May!||', 6: 'What\'s an astronaut\'s favorite part of a computer?\n||The space bar.||',
     7: 'Why are elevator jokes so good?\n||They work on many levels.||', 8: 'What do you call a fake noodle?\n||An impasta.||', 9: 'Is this pool safe for diving?\n||It deep ends.||',
-    10: 'Why don’t crabs give to charity?\n||Because they’re shellfish.||', 11: 'What’s the best way to watch a fly-fishing tournament?\n||Live stream.||',
+    10: 'Why don\'t crabs give to charity?\n||Because they\'re shellfish.||', 11: 'What\'s the best way to watch a fly-fishing tournament?\n||Live stream.||',
     12: 'How do you tell the difference between an alligator and a crocodile?\n||You will see one later and one in a while.||', 13: 'What do you call a dog that can do magic?\n||A Labracabrador.||',
-    14: 'A man walks into an apiary and asks the beekeeper for a dozen bees.\nTo which the beekeeper replies, ||"Sure, and I’ll throw in the 13th as a freebie."||', 
+    14: 'A man walks into an apiary and asks the beekeeper for a dozen bees.\nTo which the beekeeper replies, ||"Sure, and I\'ll throw in the 13th as a freebie."||', 
     15: 'What do you call a fish with no eye?\n||A fsh.||', 16: 'Justice is a dish best served cold.\n||If it were served warm, it would be justwater.||', 17: 'A steak pun is a rare medium done well.',
-    18: 'Not sure if you have noticed, but I love bad puns.\n||That’s just how eye roll.||', 19: 'Do I enjoy making courthouse puns?\n||Guilty.'
+    18: 'Not sure if you have noticed, but I love bad puns.\n||That\'s just how eye roll.||', 19: 'Do I enjoy making courthouse puns?\n||Guilty.'
 }
